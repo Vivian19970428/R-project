06fd742f80bbc0e1fdface057d24bcc8c1729d67
ShangWeiKuo <kick618ass@yahoo.com.tw>
2019-06-03 22:55:38 +0800

    資料更新

--- data_clean.R
+++ data_clean.R
@@ -2,11 +2,13 @@
 seg_le_pct <- seg_le_pct[seg_le_pct$產業別 != "13其他", ]
 seg_me_pct <- seg_me_pct[seg_me_pct$產業別 != "13其他", ]
 seg_se_pct <- seg_se_pct[seg_se_pct$產業別 != "13其他", ]
+row_se_pct <- c("201704","11投資業","0%")
+seg_se_pct <- rbind(seg_se_pct[1:394, ], row_se_pct, seg_se_pct[1:395, ])
 loan_industry2014 <- loan_industry2014[loan_indutry2014$產業別 != "13其他", ]
 
 #合併成單一資料
-total_loan_seg_industry <- cbind(seg_tseotc_pct$資料年月, 
-                                 seg_tseotc_pct$產業別,
+total_loan_seg_industry <- cbind("資料年月" = seg_tseotc_pct$資料年月, 
+                                 "產業別" = seg_tseotc_pct$產業別,
                                  "上市櫃企業-企業授信金額產業別佔比" = seg_tseotc_pct$上市櫃企業.企業授信金額產業別佔比, 
                                  "大型企業-企業授信金額產業別佔比" = seg_le_pct$大型企業.企業授信金額產業別佔比, 
                                  "中型企業-企業授信金額產業別佔比" = seg_me_pct$中型企業.企業授信金額產業別佔比, 
--- data_pepare.R
+++ data_pepare.R
@@ -1,7 +1,9 @@
 #讀入企業貸款中「產業類」的資料
-loan_indutry2014 <- loan_indutry[352:1053, ] #存放201408-201901的資料
+loan_industry <- read.csv("C:/Users/lol/Desktop/R/R-project/corporate_loan_industry.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+loan_indutry2014 <- loan_industry[352:1053, ] #存放201408-201901的資料
 
 #讀入企業貸款中「貸款規模類」的資料
+loan_scale <- read.csv("C:/Users/lol/Desktop/R/R-project/corporate_loan_scale.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 loan_scale2014 <- loan_scale[109:324, ] #存放201408-201901的資料
 
 #讀入企業貸款中「大型企業之產業授信金額占比」的資料
